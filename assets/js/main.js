b63054186605d8071c3173aa4fd6270c88f2d248
ikramarenko1 <il.kramarenko1@gmail.com>
2022-12-20 23:37:17 +0200

    added nav-menu

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,7 +1,29 @@
-/*=============== SHOW MENU ===============*/
+/*=============== Show Menu ===============*/
+const navMenu = document.getElementById('nav-menu')
+const navToggle = document.getElementById('nav-toggle')
+const navClose = document.getElementById('nav-close')
 
+if (navToggle) {
+	navToggle.addEventListener('click', () => {
+		navMenu.classList.add('show-menu')
+	})
+}
 
-/*=============== REMOVE MENU MOBILE ===============*/
+if (navClose) {
+	navClose.addEventListener('click', () => {
+		navMenu.classList.remove('show-menu')
+	})
+}
+
+/*=============== Remove Menu by clicking on the link MOBILE ===============*/
+const navLink = document.querySelectorAll('.nav__link')
+
+const linkAction = () => {
+	const navMenu = document.getElementById('nav-menu')
+	navMenu.classList.remove('show-menu')
+}
+
+navLink.forEach(n => n.addEventListener('click', linkAction))
 
 
 /*=============== CHANGE BACKGROUND HEADER ===============*/
